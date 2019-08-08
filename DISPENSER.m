4cf7aedc24e9c2807ce9757ab8b2fc1995a6fbe5
gitHleon <45692954+gitHleon@users.noreply.github.com>
2019-08-08 16:36:41 +0200

    Create DISPENSER.m

---
+++ DISPENSER.m
@@ -0,0 +1,23 @@
+classdef DISPENSER
+    %UNTITLED2 ultimus V glue dispenser
+    %   This class provides full control over Ultimus V glue dispenser
+    
+    properties
+        Property1
+    end
+    
+    methods
+        function obj = untitled2(inputArg1,inputArg2)
+            %UNTITLED2 Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.Property1 = inputArg1 + inputArg2;
+        end
+        
+        function outputArg = method1(obj,inputArg)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            outputArg = obj.Property1 + inputArg;
+        end
+    end
+end
+
