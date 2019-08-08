8869d723748b53b1e16e7dbff227fa43d6d80df0
gitHleon <45692954+gitHleon@users.noreply.github.com>
2019-08-08 16:36:51 +0200

    Create CALIBRATION.m

---
+++ CALIBRATION.m
@@ -0,0 +1,23 @@
+classdef CALIBRATION
+    %CALIBRATION class that performs the calibrations of the camera (px/um and angle)
+   
+    
+    properties
+        Property1
+    end
+    
+    methods
+        function obj = untitled10(inputArg1,inputArg2)
+            %UNTITLED10 Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.Property1 = inputArg1 + inputArg2;
+        end
+        
+        function outputArg = method1(obj,inputArg)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            outputArg = obj.Property1 + inputArg;
+        end
+    end
+end
+
