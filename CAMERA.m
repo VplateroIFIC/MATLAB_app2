dd65693f1cbe261eb24972944563abf929d91ac6
gitHleon <45692954+gitHleon@users.noreply.github.com>
2019-08-13 13:12:33 +0200

    Update CAMERA.m

--- CAMERA.m
+++ CAMERA.m
@@ -5,24 +5,24 @@ classdef CAMERA
 ReturnedColor='grayscale';
 ROIPos=[0 0 3856 2764];
 ExposureM = 'manual';
-ImageOutput='tests\images_CAMERA_class\';
+ImageOutput='tests\images\';
 cam;
 CamStatus;
     end
     
     methods
         
-        function Connect(this)
+        function this=Connect(this)
             %METHOD1 Camera connection, setting all properties
            % creating camera object, opening preview % 
            imaqreset
            this.cam = videoinput('winvideo',1);
            % settting properties of camera object %
-           this.cam.src = getselectedsource(this);
+           src = getselectedsource(this.cam);
            this.cam.ReturnedColorSpace=this.ReturnedColor;
            this.cam.ROIPosition=this.ROIPos;
-           this.cam.src.ExposureMode = this.ExposureM; 
-           
+           src.ExposureMode = this.ExposureM; 
+           disp('connection done')
         end
         function Disconnect(this)
            % deleting camera object%
@@ -31,38 +31,26 @@ CamStatus;
         end
         function Image = OneFrame(this)
             %OneFrame return current frame of the camera (image)%
-            Image=getsnapshot(this);
+            Image=getsnapshot(this.cam);
         end
         function DispFrame(this)
             % DispFrame Display current frame in a figure %
-            pic=getsnapshot(this);
+            pic=getsnapshot(this.cam);
             imshow(pic);
         end
-        function this=DispCam(this)
+        function DispCam(this)
             %DispCam display camera video in a independent windows
-            numFrame=0;
-            this.CamStatus=1;
-        while (this.CamStatus==1)
-            numFrame = numFrame+1;
-            [frame,ts] = snapshot(this);
-            frameRate = 1./(ts-ts1);
-            ts1 = ts;
-
-            showText = sprintf('frameRate:%d\n  Frame:%d\n',round(frameRate),numFrame);
-            draw = insertText(frame,[10,20],showText,'FontSize',20);
-            imshow(draw);
-            drawnow;
-        end
-        close;
+        figure('Name', 'Camera Display'); 
+        uicontrol('String', 'Close', 'Callback', 'close(gcf)'); 
+        vidRes = this.cam.VideoResolution; 
+        nBands = this.cam.NumberOfBands; 
+        hImage = image( zeros(vidRes(2), vidRes(1), nBands) ); 
+        preview(this.cam, hImage); 
         end
         
-        function this=DispCamOff(this)
+        function DispCamOff(this)
             % DispCamOff close the display of the camera
-            this.CamStatus=0;
-        end
-        function StopDispCam(this)
-            %DispCam stop display camera
-            closepreview(this.cam);
+        close;
         end
         function SaveFrame(this,name,format)
             %SaveFrame Save one frame in the ImagePath folder in the specified format%
@@ -74,9 +62,10 @@ CamStatus;
                 case 3
                     extension='.jpg';
             end
-            pic=getsnapshot(this);
+            pic=getsnapshot(this.cam);
             fullname=strcat(this.ImageOutput,name,extension);
             imwrite(pic,fullname);
+            disp('done')
         end
         end
 end
