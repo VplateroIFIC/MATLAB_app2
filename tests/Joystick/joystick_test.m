d6892d99b85db4ff6e9ec7ded776972b4e364513
gitHleon <45692954+gitHleon@users.noreply.github.com>
2019-08-21 11:16:20 +0200

    Update joystick_test.m

--- tests/Joystick/joystick_test.m
+++ tests/Joystick/joystick_test.m
@@ -2,11 +2,12 @@
 
 % Create the timer object %
 
-t = timer('Name','JoyTimer','ExecutionMode','fixedSpacing','StartDelay', 0,'TasksToExecute', 2);
+t = timer('Name','JoyTimer','ExecutionMode','fixedSpacing','StartDelay', 0);
 t.period=0.001;
 t.BusyMode='queue';
 t.TimerFcn = @joyControl;
-
+t.UserData = struct('Flag',zeros(21,1));
+% t.UserData = struct('counter_2',0);
 % starting the timer %
 
 start(t)
