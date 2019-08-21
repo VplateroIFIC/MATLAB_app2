829ece304cc3b984c64b73a0d9139a276ad74166
gitHleon <45692954+gitHleon@users.noreply.github.com>
2019-08-21 11:16:25 +0200

    Update JOYSTICK.m

--- JOYSTICK.m
+++ JOYSTICK.m
@@ -18,28 +18,6 @@ classdef JOYSTICK
         z1Axis=4;
         z2Axis=5;
         uAxis=6;
-        FlagX=0;
-        FlagY=0;
-        FlagZ1=0;
-        FlagZ2=0;
-        FlagU=0;
-        
-        FlagBut1=0;
-        FlagBut2=0;
-        FlagBut3=0;
-        FlagBut4=0;
-        FlagBut5=0;
-        FlagBut6=0;
-        FlagBut7=0;
-        FlagBut8=0;
-        FlagBut9=0;
-        FlagBut10=0;
-        FlagBut11=0;
-        FlagBut12=0;
-        FlagBut13=0;
-        FlagBut14=0;
-        FlagBut15=0;
-        FlagBut16=0;
     end
     
     methods
@@ -57,11 +35,9 @@ classdef JOYSTICK
         function this = Connect(this)
             %Connect Connecting joystick
             % starting timer to manage the joystick input
-
         this.gantry.MotorEnableAll;     
         this.t = timer('Name','JoyTimer','ExecutionMode','fixedSpacing','StartDelay', 0);
-%         t.UserData = struct('iteration', 0, 'timerThresh', this.threshold,'timerMaxVel',this.maxVel,'timerMinVel',this.minVel,'timerCurrentVel',this.CurrentVel);
-        this.t.period=0.001;
+        this.t.UserData = struct('FlagAxes',zeros(5,1),'FlagBut',zeros(16,1),'MaxVelocity',this.maxVel,'minVelocity',this.minVel,'Velocity',this.CurrentVel);
         this.t.BusyMode='queue';
         this.t.TimerFcn = {@this.joyControl};
         start(this.t);
@@ -78,52 +54,126 @@ classdef JOYSTICK
         function joyControl(this,tobj,event)
             % calling the joystick inpput %
            [pos, but] = mat_joy(0); 
-%             Npos=length(pos);
-%             Nbut=length(but);
+           
            % Controling of the axes: moving %
+           
             if (abs(pos(1))> this.threshold)
-%             vel=this.CurrentVel*pos(1);
-%             gantry.FreeRunX(this,vel);
-%             this.FlagX=1;
-            this.RunX(this,pos(1));
-             fprintf('X axis is moving %0.4f\n',pos(1));
+             vel=tobj.UserData.Velocity*pos(1);
+             this.gantry.FreeRunX(this,vel);
+             tobj.UserData.FlagAxes(1)=1;
+%              fprintf('X axis is moving %0.4f\n',pos(1));
             end
             
             if (abs(pos(2))> this.threshold)
-%             vel=this.timerCurrentVel*pos(2);
-%             gantry.FreeRunY(this,vel);
-%             this.FlagY=1;
-              fprintf('Y axis is moving %0.4f\n',pos(2));
+             vel=tobj.UserData.Velocity*pos(2);
+             this.gantry.FreeRunY(this,vel);
+             tobj.UserData.FlagAxes(2)=1;
+%               fprintf('Y axis is moving %0.4f\n',pos(2));
             end
             
-            if (abs(pos(3))> this.threshold)
-%             vel=this.timerCurrentVel*pos(3);
-%             gantry.FreeRunZ1(this,vel);
-%            this.FlagZ1=1;
-             fprintf('Z axis is moving %0.4f\n',pos(3));
+            if (abs(pos(3))> this.threshold) && (tobj.UserData.FlagAxes(5)==0) && (tobj.UserData.FlagAxes(4)==0)
+            vel=tobj.UserData.Velocity*pos(3);
+            this.gantry.FreeRunZ1(this,vel);
+            tobj.UserData.FlagAxes(3)=1;
+%              fprintf('Z axis is moving %0.4f\n',pos(3));
+            end
+            
+            if (abs(pos(3))> this.threshold) && (tobj.UserData.FlagAxes(5)==0) && (tobj.UserData.FlagAxes(4)==1)
+            vel=tobj.UserData.Velocity*pos(3);
+            this.gantry.FreeRunZ2(this,vel);
+            tobj.UserData.FlagAxes(3)=1;
+%              fprintf('Z axis is moving %0.4f\n',pos(3));
+            end
+            
+             if (abs(pos(3))> this.threshold) && (tobj.UserData.FlagAxes(5)==1)
+            vel=tobj.UserData.Velocity*pos(3);
+            this.gantry.FreeRunU(this,vel);
+            tobj.UserData.FlagAxes(3)=1;
+%              fprintf('Z axis is moving %0.4f\n',pos(3));
             end
             
             % Controling of the axes: stopping %
-            
-            if (abs(pos(1))<this.threshold) && (this.FlagX==1)
-%             gantry.MotionStop(this,this.xAxis);
-%             this.FlagX=0;
+
+            if (abs(pos(1))<this.threshold) && (tobj.UserData.FlagAxes(1)==1)
+            this.gantry.MotionStop(this,this.xAxis);
+            tobj.UserData.FlagAxes(1)=0;
             end
             
-            if (abs(pos(2))<this.threshold) && (this.FlagY==1)
-%             gantry.MotionStop(this,this.yAxis);
-%             this.FlagY=0;
+            if (abs(pos(2))<this.threshold) && (tobj.UserData.FlagAxes(2)==1)
+            this.gantry.MotionStop(this,this.yAxis);
+            tobj.UserData.FlagAxes(2)=0;
             end
             
-            if (abs(pos(3))<this.threshold) && (this.FlagZ1==1)
-%             gantry.MotionStop(this,this.z1Axis);
-%             this.FlagZ1=0;
+            if (abs(pos(3))<this.threshold) && (tobj.UserData.FlagAxes(3)==1) && (tobj.UserData.FlagAxes(5)==0) && (tobj.UserData.FlagAxes(4)==0)
+            this.gantry.MotionStop(this,this.z1Axis);
+            tobj.UserData.FlagAxes(3)=0;
             end
+            
+             if (abs(pos(3))<this.threshold) && (tobj.UserData.FlagAxes(3)==1) && (tobj.UserData.FlagAxes(5)==0) && (tobj.UserData.FlagAxes(4)==1)
+            this.gantry.MotionStop(this,this.z2Axis);
+            tobj.UserData.FlagAxes(3)=0;
+             end
+            
+             if (abs(pos(3))<this.threshold) && (tobj.UserData.FlagAxes(3)==1) && (tobj.UserData.FlagAxes(5)==1)
+            this.gantry.MotionStop(this,this.uAxis);
+            tobj.UserData.FlagAxes(3)=0;
+             end
+            
+            % Checking the buttons %
+
+            % Button 11: change between ZZ axis %
+            
+             if (but(11)==1) && (tobj.UserData.FlagBut(11)==0)
+             switch tobj.UserData.FlagAxes(4)
+                 case 0
+                     tobj.UserData.FlagAxes(4)=1;
+                 case 1
+                     tobj.UserData.FlagAxes(4)=0;
+             end
+             tobj.UserData.FlagBut(11)=1;
+             end
+             
+             if (but(11)==0) && (tobj.UserData.FlagBut(11)==1)
+             tobj.UserData.FlagBut(11)=0;
+             end
+             
+             % Button 12: change Z axis --> rotative platform %
+             
+             if (but(12)==1) && (tobj.UserData.FlagBut(12)==0)
+             switch tobj.UserData.FlagAxes(5)
+                 case 0
+                     tobj.UserData.FlagAxes(5)=1;
+                 case 1
+                     tobj.UserData.FlagAxes(5)=0;
+             end
+             tobj.UserData.FlagBut(12)=1;
+             end
+             if (but(12)==0) && (tobj.UserData.FlagBut(12)==1)
+             tobj.UserData.FlagBut(12)=0;
+             end
+             
+              % Button 1: decrease velocity %
+              
+             if (but(1)==1) && (tobj.UserData.FlagBut(1)==0)
+             tobj.UserData.Velocity=max(tobj.UserData.minVelocity,tobj.UserData.Velocity-0.05*tobj.UserData.MaxVelocity);
+             tobj.UserData.FlagBut(1)=1; 
+             fprintf('Velocity Value: %2.4f\n',tobj.UserData.Velocity);
+             end    
+             if (but(1)==0) && (tobj.UserData.FlagBut(1)==1)
+             tobj.UserData.FlagBut(1)=0;
+             end
+             
+             % Button 2: Increase Velocity %
+             
+              if (but(2)==1) && (tobj.UserData.FlagBut(2)==0)
+             tobj.UserData.Velocity=min(tobj.UserData.maxVelocity,tobj.UserData.Velocity+0.5*tobj.UserData.MaxVelocity);
+             tobj.UserData.FlagBut(2)=1;    
+             fprintf('Velocity Value: %2.4f\n',tobj.UserData.Velocity);
+             end    
+             if (but(2)==0) && (tobj.UserData.FlagBut(2)==1)
+             tobj.UserData.FlagBut(2)=0;
+             end
         end
-        
-         function RunX(this,pos) 
-            vel=this.CurrentVel*pos(1);
-            this.gantry.FreeRunX(this,vel);
-    end
+
 end
 
