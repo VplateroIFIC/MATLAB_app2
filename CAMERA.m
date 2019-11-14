d93ceb7e6be79ee14b7dff60e1f2b75b329c3c69
gitHleon <45692954+gitHleon@users.noreply.github.com>
2019-11-14 11:12:16 +0100

    Update CAMERA.m

--- CAMERA.m
+++ CAMERA.m
@@ -1,40 +1,58 @@
 classdef CAMERA
     %UNTITLED Summary of this class goes here
 properties (Access=private)  
-% setting the properties of the object %        
-ReturnedColor='grayscale';
-ROIPos=[0 0 3856 2764];
-ExposureM = 'auto';
-ImageOutput='D:\Gantry\cernbox\GANTRY-IFIC\Pictures_general\F_images_newLighting\';
+          
+ReturnedColor;
+ROIPos;
+ExposureM;
+ImageOutput;
 cam;
-CamStatus;
-calibration=3.62483; %um/pixel
+calibration; 
+trigger;
+videoAdaptor;
 end
 properties (Access=public)
 IsConnected=0; 
 end
     
     methods
+        function this=CAMERA(setup)
+ %% CAMERA constructor %%
+ % generates instance and load properties script %
+        switch setup
+            case 1
+                CAMERA_properties_Gantry
+            case 2
+                CAMERA_properties_OWIS
+        end
+        
+        this.ReturnedColor=ReturnedColor; % we take gayscale images from camera
+        this.ROIPos=ROIPos;     % resolution of the camera
+        this.ExposureM =ExposureM;         % auto tuning for the exposure
+        this.ImageOutput=ImageOutput;  % folder to save the images
+        this.calibration=calibration;  %um/pixel calibration of the camera
+        this.trigger=trigger;       % manual trigger for taking images
+        this.videoAdaptor=videoAdaptor;    %
+        end
+        
  %% Connect Camera connection, setting all properties %%      
         function this=Connect(this)
            % creating camera object, opening preview % 
            imaqreset
-           this.cam = videoinput('winvideo',1);
+           this.cam = videoinput(this.videoAdaptor,1);
            % settting properties of camera object %
            src = getselectedsource(this.cam);
            this.cam.ReturnedColorSpace=this.ReturnedColor;
            this.cam.ROIPosition=this.ROIPos;
            src.ExposureMode = this.ExposureM;
            % seting manual trigger (better performance %)
-           triggerconfig(this.cam, 'manual');
+           triggerconfig(this.cam, this.trigger);
            start(this.cam)
            this.IsConnected=1;
            disp('Camera connection done');
-           
         end
         
  %%  Disconnect Camera  %%   
-        
         function this=Disconnect(this)
             stop(this.cam)
             delete(this.cam);
@@ -43,20 +61,17 @@ end
         end
         
 %%  OneFrame return current frame of the camera (image)   %%   
-        
         function Image = OneFrame(this)
             Image=getsnapshot(this.cam);
         end
         
 %%  DispFrame Display current frame in a figure   %%  
-
         function DispFrame(this)
             pic=getsnapshot(this.cam);
             imshow(pic);
         end
        
-%%  DispCam display camera video in a independent windows   %%          
-        
+%%  DispCam display camera video in a independent windows   %%             
         function DispCam(this)
         figure('Name', 'Camera Display'); 
         uicontrol('String', 'Close', 'Callback', 'close(gcf)'); 
@@ -66,14 +81,12 @@ end
         preview(this.cam, hImage); 
         end
  
-%%  DispCamOff close the display of the camera   %%          
-        
+%%  DispCamOff close the display of the camera   %%           
         function DispCamOff(this)
         close;
         end
         
 %%  SaveFrame Save one frame in the ImagePath folder in the specified format   %%   
-
         function SaveFrame(this,name,format)
             switch format
                 case 1
