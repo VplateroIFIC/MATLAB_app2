628f326bcf4e4cba41d170508afb25ea78a3a420
gitHleon <45692954+gitHleon@users.noreply.github.com>
2019-11-29 14:18:23 +0100

    Update CAMERA_properties_Gantry.m

--- cameras_config/CAMERA_properties_Gantry.m
+++ cameras_config/CAMERA_properties_Gantry.m
@@ -3,7 +3,7 @@
 % this script generate the properties needed for the CAMERA class for the Gantry Setup. It is launched in the constructor of the class. 
 
 ROIPos=[0 0 2048 1536];     % resolution of the camera
-ImageOutput='D:\Gantry\cernbox\GANTRY-IFIC\Pictures_general\F_images_newLighting\';  % folder to save the images    
+ImageOutput='C:\Users\Pablo\Desktop\output images Gantry\';  % folder to save the images    
 calibration=3.62483;  %um/pixel calibration of the camera
 videoAdaptor='gentl';    % Matlab video adaptor used
 triggerConfig='hardware';
