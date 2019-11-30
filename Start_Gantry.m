792245ea296f11bafa01e3e55752dd78b639f382
gitHleon <45692954+gitHleon@users.noreply.github.com>
2019-11-30 11:59:29 +0100

    Create Start_Gantry.m

---
+++ Start_Gantry.m
@@ -0,0 +1,16 @@
+
+% Script prepared to begin main gantry setup functions
+gantry = STAGES(2);
+gantry = gantry.Connect;
+
+joy = JOYSTICK (gantry);
+joy = joy.Connect;
+
+% gantry.MotorEnableAll
+
+fid=FIDUCIALS(1);
+
+cam= CAMERA(5);
+cam = cam.Connect;
+
+focus = FOCUS(gantry, cam);
