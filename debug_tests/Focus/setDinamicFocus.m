88f14a9456ab77ff322c4d32a9a639e02424cb4d
gitHleon <45692954+gitHleon@users.noreply.github.com>
2019-11-30 15:19:09 +0100

    Update setDinamicFocus.m

--- debug_tests/Focus/setDinamicFocus.m
+++ debug_tests/Focus/setDinamicFocus.m
@@ -1,57 +1,96 @@
-function [data, ImageTime, metadata, Z, Ztime, dataF, ImageTimeF, metadataF] = setDinamicFocus(gantry,cam,range,velocity)
+function [resultsImages] = setDinamicFocus(gantry,cam,range,velocity)
 %setDinamicFocus taking dinamic images around optimal focus to characterize focus
 %   gantry: gantry instance. It has to be connected
 %   cam: Cam instance. It has to be connected
 %   range: window where characterization will take place
 %   Velocity: Z velocity during images capturing(positive value!!) 
-
+tic
+Zaxis=4;
+% starting Z
+origin=gantry.GetPosition(Zaxis);
 %  general constants %
-outputPathResults='';   % save the txt results files
-zaxis=4;
+outputPathResults='C:\Users\Pablo\Desktop\output_images_results_Gantry\';   % save the txt results files
+
 cont=1;
 resultsFile=strcat(outputPathResults,'DinamicFocus.txt');
 % setting size of the slots for incoming data %
 data=cell(100);
 ImageTime=cell(100);
 metadata=cell(100);
-Z=zeros(100);
-Ztime=zeros(100);
+Z=zeros(100,1);
+ZtimeBefore=zeros(100,6);
+ZtimeAfter=zeros(100,6);
 % fileID = fopen(resultsFile,'w');
 
 % movin Z stage to the starting point %
-gantry.moveBy(zaxis,-range/2,velocity);
-gantry.WaitForMotion(zAxis,-1);
+gantry.MoveBy(Zaxis,-range/2,velocity);
+gantry.WaitForMotion(Zaxis,-1);
 
 % setting initial Z
-Z0=gantry.GetPosition(zaxis);
+Z0=gantry.GetPosition(Zaxis);
 
-% starting video adquisition %
+% starting video adquisition. Freezing script until we receibe the firs images!! %
 cam.startAdquisition;
+test=cam.retrieveData;
+while isempty(test==1)
+    test=cam.retrieveData;
+end
+% while flag==0
+    
 
 % starting movement %
 gantry.FreeRunZ1(velocity)
-currentPosition=gantry.GetPosition(zaxis);
+currentPosition=gantry.GetPosition(Zaxis);
 
 % starting measurement loop %
 while(abs(currentPosition-Z0)<range)
-[data{cont},ImageTime{cont},metadata{cont}]=cam.RetrieveData;
-Z(cont)=gantry.GetPosition(zaxis);
-Ztime(cont)=clock;
+% [data{cont},ImageTime{cont},metadata{cont}]=cam.RetrieveData;
+ZtimeBefore(cont,:)=clock;        % time before get position
+Z(cont)=gantry.GetPosition(Zaxis);
+ZtimeAfter(cont,:)=clock;        % time After get position
 currentPosition=Z(cont);
-dimension=size(data(cont));
-Image=data{cont}(:,:,1,dimension(4));
-nameImage=strcat('nameImage_',num2str(cont));
-cam.saveFrame(Image,nameImage,2);
+% dimension=size(data(cont));
+% Image=data{cont}(:,:,1,dimension(4));
+% nameImage=strcat('nameImage_',num2str(cont));
 cont=cont+1;
 end
 
 % stopping gantry %
-gantry.MotionStop(zaxis);
-
-% taking final camera data %
-[dataF,ImageTimeF,metadataF]=cam.RetrieveData;
+gantry.MotionStop(Zaxis);
 
 % stoping cam adquisition %
 cam.stopAdquisition
+toc
+
+% taking final camera data %
+[dataF,imageTimeF,metadataF]=cam.retrieveData;
+
+resultsImages.images=dataF;
+resultsImages.time=imageTimeF;
+resultsImages.metadata=metadataF;
+resultsImages.Z=Z;
+resultsImages.ZtimeBefore=ZtimeBefore;
+resultsImages.ZtimeAfter=ZtimeAfter;
+
+namePath=strcat(outputPathResults,'results.mat');
+save(namePath,'resultsImages')
+
+
+% taking Gantry to the starting point
+gantry.MoveTo(Zaxis,origin,0.1);
+gantry.WaitForMotion(Zaxis,-1);
+Zfinal=gantry.GetPosition(Zaxis);
+
+[resX,resY,rgb,frames]=size(dataF);
+
+
+% saving images in outup folder
+for i=1:frames
+   image=dataF(:,:,1,i);
+   nameImageFile=strcat(outputPathResults,'DinamicFocus_',num2str(i),'.png');
+   imwrite(image,nameImageFile);
+end
+
+
 
 end
