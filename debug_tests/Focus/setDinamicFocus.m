d345483e7e5dfb01eb01ec28ed6f1dd3b7f1a283
gitHleon <45692954+gitHleon@users.noreply.github.com>
2019-12-02 11:45:57 +0100

    Update setDinamicFocus.m

--- debug_tests/Focus/setDinamicFocus.m
+++ debug_tests/Focus/setDinamicFocus.m
@@ -9,18 +9,12 @@ Zaxis=4;
 % starting Z
 origin=gantry.GetPosition(Zaxis);
 %  general constants %
-outputPathResults='C:\Users\Pablo\Desktop\output_images_results_Gantry\';   % save the txt results files
-
+outputPathResults='C:\Users\Pablo\Desktop\output_images_results_Gantry\';   % save the txt results file
 cont=1;
-resultsFile=strcat(outputPathResults,'DinamicFocus.txt');
 % setting size of the slots for incoming data %
-data=cell(100);
-ImageTime=cell(100);
-metadata=cell(100);
 Z=zeros(100,1);
 ZtimeBefore=zeros(100,6);
 ZtimeAfter=zeros(100,6);
-% fileID = fopen(resultsFile,'w');
 
 % movin Z stage to the starting point %
 gantry.MoveBy(Zaxis,-range/2,velocity);
@@ -32,15 +26,12 @@ Z0=gantry.GetPosition(Zaxis);
 % starting video adquisition.
 cam.startAdquisition;
 
-    
-
 % starting movement %
 gantry.FreeRunZ1(velocity)
 currentPosition=gantry.GetPosition(Zaxis);
 
 % starting measurement loop %
 while(abs(currentPosition-Z0)<range)
-% [data{cont},ImageTime{cont},metadata{cont}]=cam.RetrieveData;
 ZtimeBefore(cont,:)=clock;        % time before get position
 Z(cont)=gantry.GetPosition(Zaxis);
 ZtimeAfter(cont,:)=clock;        % time After get position
@@ -68,22 +59,8 @@ resultsImages.ZtimeAfter=ZtimeAfter;
 namePath=strcat(outputPathResults,'results.mat');
 save(namePath,'resultsImages')
 
-
 % taking Gantry to the starting point
 gantry.MoveTo(Zaxis,origin,0.1);
 gantry.WaitForMotion(Zaxis,-1);
-Zfinal=gantry.GetPosition(Zaxis);
-
-[resX,resY,rgb,frames]=size(dataF);
-
-
-% saving images in outup folder
-for i=1:frames
-   image=dataF(:,:,1,i);
-   nameImageFile=strcat(outputPathResults,'DinamicFocus_',num2str(i),'.png');
-   imwrite(image,nameImageFile);
-end
-
-
 
 end
