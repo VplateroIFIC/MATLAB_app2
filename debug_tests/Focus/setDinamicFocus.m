ba93e845819bbe6f7842d59e03ee24c7b8e836be
gitHleon <45692954+gitHleon@users.noreply.github.com>
2019-12-04 15:26:48 +0100

    Update setDinamicFocus.m

--- debug_tests/Focus/setDinamicFocus.m
+++ debug_tests/Focus/setDinamicFocus.m
@@ -4,7 +4,7 @@ function [resultsImages] = setDinamicFocus(gantry,cam,range,velocity)
 %   cam: Cam instance. It has to be connected
 %   range: window where characterization will take place
 %   Velocity: Z velocity during images capturing(positive value!!) 
-tic
+
 Zaxis=4;
 % starting Z
 origin=gantry.GetPosition(Zaxis);
@@ -13,8 +13,7 @@ outputPathResults='C:\Users\Pablo\Desktop\output_images_results_Gantry\';   % sa
 cont=1;
 % setting size of the slots for incoming data %
 Z=zeros(100,1);
-ZtimeBefore=zeros(100,6);
-ZtimeAfter=zeros(100,6);
+
 
 % movin Z stage to the starting point %
 gantry.MoveBy(Zaxis,-range/2,velocity);
@@ -25,6 +24,7 @@ Z0=gantry.GetPosition(Zaxis);
 
 % starting video adquisition.
 cam.startAdquisition;
+tic
 
 % starting movement %
 gantry.FreeRunZ1(velocity)
@@ -32,9 +32,9 @@ currentPosition=gantry.GetPosition(Zaxis);
 
 % starting measurement loop %
 while(abs(currentPosition-Z0)<range)
-ZtimeBefore(cont,:)=clock;        % time before get position
+ZtimeBefore(cont)=toc;        % time before get position
 Z(cont)=gantry.GetPosition(Zaxis);
-ZtimeAfter(cont,:)=clock;        % time After get position
+ZtimeAfter(cont)=toc;        % time After get position
 currentPosition=Z(cont);
 cont=cont+1;
 end
@@ -44,10 +44,10 @@ gantry.MotionStop(Zaxis);
 
 % stoping cam adquisition %
 cam.stopAdquisition
-toc
+
 
 % taking final camera data %
-[dataF,imageTimeF,metadataF]=cam.retrieveData;
+[dataF,imageTimeF,metadataF]=cam.retrieveDataAllFrames;
 
 resultsImages.images=dataF;
 resultsImages.time=imageTimeF;
