09c6fd199d6ec895e22f33ded9cbc87d2f38f59c
gitHleon <45692954+gitHleon@users.noreply.github.com>
2019-12-04 15:26:45 +0100

    Update setStaticFocus.m

--- debug_tests/Focus/setStaticFocus.m
+++ debug_tests/Focus/setStaticFocus.m
@@ -1,4 +1,4 @@
-function setStaticFocus(gantry,cam,range,Nimages)
+function results=setStaticFocus(gantry,cam,range,Nimages)
 %setStaticFocus taking static images around optimal focus to characterize focus
 % Gantry and camera objects have to be created and the devices have to be connected
 %   gantry: gantry instance. It has to be connected
@@ -19,24 +19,42 @@ gantry.WaitForMotion(Zaxis,-1);
 cam.startAdquisition;
 
 % Starting the measure loop %
-Z=zeros(Nimages);
-images=zeros(2048,1536,1,Nimages);
+Z=zeros(Nimages,1);
+images=zeros(1536,2048,Nimages);
 for i=1:Nimages
-    [data,time,medatada]=cam.retrieveData;
+    cam.ResetAdquisitionBuffer;
+    [data,time,medatada]=cam.retrieveDataAllFrames;
+    if length(size(data))==4
     [resx,resy,color,frame]=size(data);
-    images(:,:,Nimages)=data(:,:,1,frame);
+    else
+    frame=1;
+    end
+    images(:,:,i)=data(:,:,1,frame);
     Z(i)=gantry.GetPosition(Zaxis);
     gantry.MoveBy(Zaxis,delta,velocity);
     gantry.WaitForMotion(Zaxis,-1);
+    
+    message=(['frame number ',int2str(i),' was taken']);
+    disp(message)
 end
- gantry.MoveTo(Zaxis,Z0,velocity);
+cam.stopAdquisition
+
+gantry.MoveTo(Zaxis,Z0,velocity);
  gantry.WaitForMotion(Zaxis,-1);
  
  % saving results %
  results.Images=images;
  results.Zvalues=Z;
+ 
+%  for i=1:length(Z)
+%      imOut=images(:,:,i);
+%      imOutGray=mat2gray(imOut);
+%      imOut_uint8=uint8(imOutGray);
+%      namePic=([])
+%      imwrite()
+ 
  namePath=strcat(outputPathResults,'results.mat');
- save(namePath,'results')
+ save(namePath,'results','-v7.3')
  
 end
 
