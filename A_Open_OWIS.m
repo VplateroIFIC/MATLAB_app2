ec36313a41b33563a8cb582b865b3ff906ea1eaa
VplateroIFIC <55190148+VplateroIFIC@users.noreply.github.com>
2020-01-30 15:46:27 +0100

    Update A_Open_OWIS.m

--- A_Open_OWIS.m
+++ A_Open_OWIS.m
@@ -1,10 +1,10 @@
 % addpath('C:\Users\leon0\Documents\Matlab_app\owis_lib\x64')
 eje = OWIS_STAGES;
 eje = eje.INIT;
-joy = JOYSTICK_OWIS(eje);
-joy = joy.Connect;
-cam=CAMERA(4);
-cam=cam.Connect;
-cam.DispCam
-focus=FOCUS(eje,cam,2);
-fid=FICUCIALS(2);
+% joy = JOYSTICK_OWIS(eje);
+% joy = joy.Connect;
+% cam=CAMERA(4);
+% cam=cam.Connect;
+% cam.DispCam
+% focus=FOCUS(eje,cam,2);
+% fid=FICUCIALS(2);
