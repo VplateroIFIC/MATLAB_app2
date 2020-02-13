050f0d5111264e169b6da989f49be075384464e8
VplateroIFIC <55190148+VplateroIFIC@users.noreply.github.com>
2020-02-13 13:14:54 +0100

    Update QR_reader.m

--- QR_reader.m
+++ QR_reader.m
@@ -1,28 +1,31 @@
 
+TimeOut = 15;
+out = ' ';
+ExitCommand = 'END';
+ComPort = 'COM3';
 
-QRreader = serial('COM7','BaudRate',9600,'DataBits',8,'Terminator','CR','BytesAvailableFcnMode','byte') %creating serial port object
+delete(instrfindall);
+QRreader = serial(ComPort,'BaudRate',9600,'DataBits',8,'Terminator','CR','BytesAvailableFcnMode','byte') %creating serial port object
 fopen(QRreader)
 readasync(QRreader)             %Reading in async mode. Read input buffer as soon as new data is available.
-out = ' ';
-fileID = fopen('QRreaded.txt','a');     % 'w' writting or 'a' append;
-% fprintf(fileID,'#################\n', datetime);
+fileID = fopen('QRcodes.txt','a');         % 'w' writting or 'a' append;
 fprintf(fileID,'## %s ##\n', datetime);
 disp ('Ready to Scan\n');
 tic;
-while (strcmp(out,'END') == 0) && (toc < TimeOut)
-%     out = fscanf(QRreader)
-%     out = fgets(QRreader
-    out = fgetl(QRreader);
+while (strcmp(out, ExitCommand) == 0) && (toc < TimeOut)
+%     out = fscanf(QRreader);               % Read ASCII data from device, and format as text
+%     out = fgets(QRreader);                % Read line of text from device and include terminator
+    out = fgetl(QRreader);                  % Read line of ASCII text and discard terminator
     
-    if (isempty(out)) == 0              % If some input arrived:
-        tic;                            % Reset timeout timer
-        fprintf(fileID,'%s \n', out);   % Print date to the file
+    if (isempty(out)) == 0                  % If some input arrived:
+        tic;                                % Reset timeout timer
+        fprintf(fileID,'%s \n', out);       % Write readed code to the file
         fprintf('Readed QRcode: %s \n', out);
     end  
 end
-fprintf(fileID,'\n', datetime);
+fprintf(fileID,'\n');
 
 fclose(fileID);
-stopasync(QRreader)
-fclose(QRreader)
+stopasync(QRreader);
+fclose(QRreader);
 disp ('Finish reading');
