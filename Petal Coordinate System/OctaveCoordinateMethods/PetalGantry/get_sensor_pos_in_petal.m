e342ff4f35318e3a931e266b6cf5352dac8d4d42
gitHleon <45692954+gitHleon@users.noreply.github.com>
2020-02-28 16:07:18 +0100

    Update get_sensor_pos_in_petal.m

--- Petal Coordinate System/OctaveCoordinateMethods/PetalGantry/get_sensor_pos_in_petal.m
+++ Petal Coordinate System/OctaveCoordinateMethods/PetalGantry/get_sensor_pos_in_petal.m
@@ -2,5 +2,5 @@
 % Return sensor coordinates in petal
 %
 function pos = get_sensor_pos_in_petal(petal, indx)
-   os = petal.sensor_positions(:,indx);
-endfunction
+   pos = petal.sensor_positions(:,indx);
+end
