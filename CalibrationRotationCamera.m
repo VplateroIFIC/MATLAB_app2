75b750bc65319073a6fa1b449711e2d68f53e33a
gitHleon <45692954+gitHleon@users.noreply.github.com>
2020-06-22 13:27:08 +0200

    Create CalibrationRotationCamera.m

---
+++ CalibrationRotationCamera.m
@@ -0,0 +1,23 @@
+function angle = CalibrationRotationCamera(gantry, cam, fid, direction, Nimages, gap)
+%UNTITLED2 This function calculat the angle of the offset camera-gantry
+%   A set of images of the fiducial are taken. the calculated centers are fitted into a line, and the angle with the orizontal axis is calculated
+% The F fiducial has to be seen during all the movements. The inicial position is important.
+% A focused image is required to the proper identification of the fiducial. Focus before run the function.
+% inputs
+%   gantry: gantry object
+%   camera: camera object
+%   fid: fiducials object
+%   direction: 0 for X direction, 1 for Y direction
+%   Nimages: Number of images taken
+%   gap: separation between images
+% outputs
+%   angle: angle offset camera-gantry.
+
+
+
+
+
+
+
+end
+
