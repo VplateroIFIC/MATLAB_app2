8e17916ab6ec24b3ace7b441f917cb093b554407
gitHleon <45692954+gitHleon@users.noreply.github.com>
2020-07-15 13:36:11 +0200

    Create MultiShot.vb

---
+++ Lasers/scan/scanCONTROL Windows SDK 4.0/VB.NET SDK/src/MultiShot.vb
@@ -0,0 +1,357 @@
+﻿Imports System
+Imports System.Runtime.InteropServices
+Imports System.Text
+Imports System.Threading
+
+
+Namespace VB
+
+    Public Class VBscanCNONTROLSample
+
+        ' Global variables
+        Const MAX_INTERFACE_COUNT As Integer = 5
+        Const Max_RESOLUTIONS As Integer = 6
+
+        Public Shared uiResolution As UInteger = 0
+        Public Shared hLLT As UInteger = 0
+        Public Shared tscanCONTROLType As CLLTI.TScannerType
+
+        Public Shared abyProfileBuffer As Byte()
+
+        Public Shared bProfileReceived As UInteger = 0
+        Public Shared uiNeededProfileCount As UInteger = 10
+        Public Shared uiProfileDataSize As UInteger = 0
+        Public Shared m_uiProfileCount As UInteger = 0
+
+
+
+        ' Define an array with two AutoResetEvent WaitHandles.
+        Shared hProfileEvent As AutoResetEvent = New AutoResetEvent(False)
+
+        'TOCHECK: STATTHREAD
+        Shared Sub Main()
+            scanCONTROL_Sample()
+        End Sub
+
+        Shared Sub scanCONTROL_Sample()
+            Dim auiInterfaces(MAX_INTERFACE_COUNT - 1) As UInteger
+            Dim auiResolutions(Max_RESOLUTIONS - 1) As UInteger
+
+            Dim sbDevName As StringBuilder = New StringBuilder(100)
+            Dim sbVenName As StringBuilder = New StringBuilder(100)
+
+            Dim uiBufferCount As UInteger = 20
+            Dim uiMainReflection As UInteger = 0
+            Dim uiPacketSize As UInteger = 1024
+
+            Dim iInterfaceCount As Integer = 0
+            Dim uiShutterTime As UInteger = 100
+            Dim uiIdleTime As UInteger = 900
+            Dim iRetValue As Integer
+            Dim bOK As Boolean = True
+            Dim bConnected As Boolean = False
+            Dim cki As ConsoleKeyInfo
+
+            Console.WriteLine("----- Connect to scanCONTROL -----" & Environment.NewLine)
+
+            ' Create an Ethernet Device -> returns handle to LLT device
+            hLLT = CLLTI.CreateLLTDevice(CLLTI.TInterfaceType.INTF_TYPE_ETHERNET)
+            If (hLLT <> 0) Then
+                Console.WriteLine("CreateLLTDevice OK")
+            Else
+                Console.WriteLine("Error during CreateLLTDevice" & Environment.NewLine)
+            End If
+
+            Console.WriteLine("----- Connect to scanCONTROL -----" & Environment.NewLine)
+
+            ' Gets the available interfaces from the scanCONTROL handle
+            iInterfaceCount = CLLTI.GetDeviceInterfacesFast(hLLT, auiInterfaces, auiInterfaces.GetLength(0))
+            If (iInterfaceCount <= 0) Then
+                Console.WriteLine("FAST: There is no scanCONTROL connected")
+            ElseIf (iInterfaceCount = 1) Then
+                Console.WriteLine("FAST: There is 1 scanCONTROL connected ")
+            Else
+                Console.WriteLine("FAST: There are " & iInterfaceCount & " scanCONTROL's connected")
+            End If
+
+            If (iInterfaceCount >= 1) Then
+                Dim target4 As UInteger = auiInterfaces(0) And &HFF
+                Dim target3 As UInteger = (auiInterfaces(0) And &HFF00) >> 8
+                Dim target2 As UInteger = (auiInterfaces(0) And &HFF0000) >> 16
+                Dim target1 As UInteger = (auiInterfaces(0) And &HFF000000) >> 24
+
+                ' Set the first IP address detected by GetDeviceInterfacesFast to handle
+                Console.WriteLine("Select the device interface: " & target1 & "." & target2 & "." & target3 & "." & target4)
+                iRetValue = CLLTI.SetDeviceInterface(hLLT, auiInterfaces(0), 0)
+                If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+                    OnError("Error during SetDeviceInterface", iRetValue)
+                    bOK = False
+                End If
+
+                If (bOK) Then
+                    ' Connect to sensor with the device interface set before
+                    Console.WriteLine("Connecting to scanCONTROL")
+                    iRetValue = CLLTI.Connect(hLLT)
+                    If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+
+                        OnError("Error during Connect", iRetValue)
+                        bOK = False
+
+                    Else
+                        bConnected = True
+                    End If
+                End If
+
+                If (bOK) Then
+                    Console.WriteLine(Environment.NewLine & "----- Get scanCONTROL Info -----" & Environment.NewLine)
+                    ' Read the device name and vendor from scanner
+                    Console.WriteLine("Get Device Name")
+                    iRetValue = CLLTI.GetDeviceName(hLLT, sbDevName, sbDevName.Capacity, sbVenName, sbVenName.Capacity)
+                    If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+                        OnError("Error during GetDevName", iRetValue)
+                        bOK = False
+                    Else
+                        Console.WriteLine(" - Devname: " & sbDevName.ToString & Environment.NewLine & " - Venname: " & sbVenName.ToString)
+                    End If
+                End If
+
+                If (bOK) Then
+
+                    ' Get the scanCONTROL type and check if it is valid
+                    Console.WriteLine("Get scanCONTROL type")
+                    iRetValue = CLLTI.GetLLTType(hLLT, tscanCONTROLType)
+                    If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+                        OnError("Error during GetLLTType", iRetValue)
+                        bOK = False
+                    End If
+
+                    If (iRetValue = CLLTI.GENERAL_FUNCTION_DEVICE_NAME_NOT_SUPPORTED) Then
+                        Console.WriteLine(" - Can't decode scanCONTROL type. Please contact Micro-Epsilon for a newer version of the LLT.dll.")
+                    End If
+
+                    If (tscanCONTROLType >= CLLTI.TScannerType.scanCONTROL27xx_25 And tscanCONTROLType <= CLLTI.TScannerType.scanCONTROL27xx_xxx) Then
+                        Console.WriteLine(" - The scanCONTROL is a scanCONTROL27xx")
+                    ElseIf (tscanCONTROLType >= CLLTI.TScannerType.scanCONTROL25xx_25 And tscanCONTROLType <= CLLTI.TScannerType.scanCONTROL25xx_xxx) Then
+                        Console.WriteLine(" - The scanCONTROL is a scanCONTROL25xx")
+                    ElseIf (tscanCONTROLType >= CLLTI.TScannerType.scanCONTROL26xx_25 And tscanCONTROLType <= CLLTI.TScannerType.scanCONTROL26xx_xxx) Then
+                        Console.WriteLine(" - The scanCONTROL is a scanCONTROL26xx")
+                    ElseIf (tscanCONTROLType >= CLLTI.TScannerType.scanCONTROL29xx_25 And tscanCONTROLType <= CLLTI.TScannerType.scanCONTROL29xx_xxx) Then
+                        Console.WriteLine(" - The scanCONTROL is a scanCONTROL29xx")
+                    ElseIf (tscanCONTROLType >= CLLTI.TScannerType.scanCONTROL30xx_25 And tscanCONTROLType <= CLLTI.TScannerType.scanCONTROL30xx_xxx) Then
+                        Console.WriteLine(" - The scanCONTROL is a scanCONTROL30xx")
+                    Else
+                        Console.WriteLine(" - The scanCONTROL is a undefined type!" & Environment.NewLine & "Please contact Micro-Epsilon for a newer SDK!")
+                    End If
+
+                    ' Get all possible resolutions for connected sensor and save them in array 
+                    Console.WriteLine("Get all possible resolutions")
+                    iRetValue = CLLTI.GetResolutions(hLLT, auiResolutions, auiResolutions.GetLength(0))
+                    If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+                        OnError("Error during GetResolutions", iRetValue)
+                        bOK = False
+                    End If
+
+                    ' Set the max. possible resolution
+                    uiResolution = auiResolutions(0)
+                End If
+
+                ' Set scanner settings to valid parameters for this example                
+                If (bOK) Then
+                    Console.WriteLine(Environment.NewLine & "----- Set scanCONTROL Parameters -----" & Environment.NewLine)
+                    Console.WriteLine("Set resolution to " & uiResolution)
+                    iRetValue = CLLTI.SetResolution(hLLT, uiResolution)
+                    If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+                        OnError("Error during SetResolution", iRetValue)
+                        bOK = False
+                    End If
+                End If
+
+                If (bOK) Then
+                    Console.WriteLine("Set BufferCount to " & uiBufferCount)
+                    iRetValue = CLLTI.SetBufferCount(hLLT, uiBufferCount)
+                    If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+                        OnError("Error during SetBufferCount", iRetValue)
+                        bOK = False
+                    End If
+                End If
+
+                If (bOK) Then
+                    Console.WriteLine("Set MainReflection to " & uiMainReflection)
+                    iRetValue = CLLTI.SetMainReflection(hLLT, uiMainReflection)
+                    If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+                        OnError("Error during SetMainReflection", iRetValue)
+                        bOK = False
+                    End If
+                End If
+
+                If (bOK) Then
+                    Console.WriteLine("Set Packetsize to " & uiPacketSize)
+                    iRetValue = CLLTI.SetPacketSize(hLLT, uiPacketSize)
+                    If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+                        OnError("Error during SetPacketSize", iRetValue)
+                        bOK = False
+                    End If
+                End If
+
+                If (bOK) Then
+                    Console.WriteLine("Set Profile config to PROFILE")
+                    iRetValue = CLLTI.SetProfileConfig(hLLT, CLLTI.TProfileConfig.PROFILE)
+                    If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+                        OnError("Error during SetProfileConfig", iRetValue)
+                        bOK = False
+                    End If
+                End If
+
+
+
+                If (bOK) Then
+                    Console.WriteLine("Set trigger to internal")
+                    iRetValue = CLLTI.SetFeature(hLLT, CLLTI.FEATURE_FUNCTION_TRIGGER, CLLTI.TRIG_INTERNAL)
+                    If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+                        OnError("Error during SetFeature(FEATURE_FUNCTION_TRIGGER)", iRetValue)
+                        bOK = False
+                    End If
+                End If
+
+                If (bOK) Then
+                    Console.WriteLine("Set shutter time to " & uiShutterTime)
+                    iRetValue = CLLTI.SetFeature(hLLT, CLLTI.FEATURE_FUNCTION_EXPOSURE_TIME, uiShutterTime)
+                    If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+                        OnError("Error during SetFeature(FEATURE_FUNCTION_SHUTTERTIME)", iRetValue)
+                        bOK = False
+                    End If
+                End If
+
+                If (bOK) Then
+                    Console.WriteLine("Set idle time to " & uiIdleTime)
+                    iRetValue = CLLTI.SetFeature(hLLT, CLLTI.FEATURE_FUNCTION_IDLETIME, uiIdleTime)
+                    If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+                        OnError("Error during SetFeature(FEATURE_FUNCTION_IDLETIME)", iRetValue)
+                        bOK = False
+                    End If
+                End If
+
+                ' Setup callback
+                If (bOK) Then
+                    Console.WriteLine(Environment.NewLine & "----- Setup Callback function and event -----" & Environment.NewLine)
+
+                    Console.WriteLine("Register the callback")
+                    ' Set the callback function
+                    Dim gch As GCHandle = GCHandle.Alloc(New CLLTI.ProfileReceiveMethod(AddressOf ProfileEvent))
+
+                    ' Register the callback
+                    iRetValue = CLLTI.RegisterCallback(hLLT, CLLTI.TCallbackType.STD_CALL, DirectCast(gch.Target, CLLTI.ProfileReceiveMethod), hLLT)
+
+                    'gch.Free()
+                    If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+                        OnError("Error during RegisterCallback", iRetValue)
+                        bOK = False
+                    End If
+                End If
+
+                ' Main tasks in this example
+                If (bOK) Then
+                    Console.WriteLine(Environment.NewLine & "----- Get profiles with Callback from scanCONTROL -----" & Environment.NewLine)
+                    Multi_Shot()
+                End If
+
+                Console.WriteLine(Environment.NewLine & "----- Disconnect from scanCONTROL -----" & Environment.NewLine)
+
+                If (bConnected) Then
+                    ' Disconnect from the sensor
+                    Console.WriteLine("Disconnect the scanCONTROL")
+                    iRetValue = CLLTI.Disconnect(hLLT)
+                    If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+                        OnError("Error during Disconnect", iRetValue)
+                    End If
+                End If
+
+                If (bConnected) Then
+                    ' Free ressources
+                    Console.WriteLine("Delete the scanCONTROL instance")
+                    iRetValue = CLLTI.DelDevice(hLLT)
+                    If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+                        OnError("Error during Delete", iRetValue)
+                    End If
+                End If
+            End If
+
+            'Wait for a keyboard hit
+            While (True)
+                cki = Console.ReadKey()
+                If (cki.KeyChar <> "0") Then
+                    Exit While
+                End If
+            End While
+
+        End Sub
+
+
+        ' Evalute received profiles via callback function
+        Shared Sub Multi_Shot()
+
+            Dim iRetValue As Integer
+
+            ' Start continous profile transmission
+            Console.WriteLine("Enable the measurement")
+            iRetValue = CLLTI.TransferProfiles(hLLT, CLLTI.TTransferProfileType.SHOT_TRANSFER, 1)
+            If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+                OnError("Error during TransferProfiles", iRetValue)
+                Return
+            End If
+
+            'Get 10 profiles
+            iRetValue = CLLTI.MultiShot(hLLT, 10)
+            If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+                OnError("Error during MultiShot", iRetValue)
+                Return
+            End If
+
+            'Wait for the profiles to be received
+            System.Threading.Thread.Sleep(500)
+
+            'Check if correct number of profiles have been received
+            If (m_uiProfileCount <> 10) Then
+                Console.WriteLine("Wrong profile count received")
+                Return
+            End If
+
+            Console.WriteLine("Received 10 profiles - OK")
+
+
+            ' Stop shot profile transmission
+            Console.WriteLine("Disable the measurement")
+            iRetValue = CLLTI.TransferProfiles(hLLT, CLLTI.TTransferProfileType.SHOT_TRANSFER, 0)
+            If (iRetValue < CLLTI.GENERAL_FUNCTION_OK) Then
+                OnError("Error during TransferProfiles", iRetValue)
+                Return
+            End If
+
+        End Sub
+
+
+        ' Callback function which copies the received data into the buffer and sets an event after the specified profiles
+        Shared Sub ProfileEvent(ByVal data As IntPtr, uiSize As UInteger, uiUserData As UInteger)
+
+            'Wie oft wird der Callback aufgerufen
+            m_uiProfileCount = m_uiProfileCount + 1
+
+        End Sub
+
+
+
+        ' Display the error text
+        Shared Sub OnError(strErrorTxt As String, iErrorValue As Integer)
+
+            Dim acErrorString(200) As Byte
+
+            Console.WriteLine(strErrorTxt)
+            If (CLLTI.TranslateErrorValue(hLLT, iErrorValue, acErrorString, acErrorString.GetLength(0)) _
+                                            >= CLLTI.GENERAL_FUNCTION_OK) Then
+                Console.WriteLine(System.Text.Encoding.ASCII.GetString(acErrorString, 0, acErrorString.GetLength(0)))
+            End If
+        End Sub
+
+    End Class
+
+End Namespace
